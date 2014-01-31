c464ae1ec6f7bcfde2d24e312b9ac6628e568188
ElricleNecro <guiplum@gmail.com>
2014-01-31 19:49:21 +0100

    Now use an abstract class instead of OGLCube.

--- include/oglwidget.hpp
+++ include/oglwidget.hpp
@@ -10,39 +10,44 @@
 #include <QMouseEvent>
 
 #include <iostream>
-#include "OGLCube.hpp"
+
+#include <stdexcept>
+
+//#include "OGLCube.hpp"
+#include "interface.hpp"
 
 class OGLWidget : public QGLWidget
 {
-        Q_OBJECT
-    public:
-        OGLWidget(QWidget *parent = 0);
-        QSize sizeHint() const;
+	Q_OBJECT
+	public:
+		OGLWidget(Data* fname, QWidget *parent = 0);
+		QSize sizeHint() const;
 
-    protected:
-        virtual void initializeGL();
-        virtual void resizeGL( int width, int height );
-        virtual void paintGL();
+	protected:
+		virtual void initializeGL();
+		virtual void resizeGL( int width, int height );
+		virtual void paintGL();
 
-        virtual void keyPressEvent( QKeyEvent *e );
-        virtual void wheelEvent(QWheelEvent *event);
-        virtual void mousePressEvent(QMouseEvent *event);
-        virtual void mouseReleaseEvent(QMouseEvent *e);
-        virtual void mouseMoveEvent(QMouseEvent *e);
-        virtual void timerEvent(QTimerEvent *e);
+		virtual void keyPressEvent( QKeyEvent *e );
+		virtual void wheelEvent(QWheelEvent *event);
+		virtual void mousePressEvent(QMouseEvent *event);
+		virtual void mouseReleaseEvent(QMouseEvent *e);
+		virtual void mouseMoveEvent(QMouseEvent *e);
+		virtual void timerEvent(QTimerEvent *e);
 
-    private:
-        QGLShaderProgram program;
-        QBasicTimer *timer;
-        OGLCube::OGLCube cube;
-        QMatrix4x4 camera;
-        QMatrix4x4 projection, model, view;
-        QPoint lastMousePosition;
-        QQuaternion rotation;
-        QVector2D mousePressPosition;
-        QVector3D rotationAxis;
-        qreal angularSpeed;
-        double distance;
+	private:
+		QGLShaderProgram program;
+		QBasicTimer *timer;
+		//OGLCube::OGLCube cube;
+		Data *cube;
+		QMatrix4x4 camera;
+		QMatrix4x4 projection, model, view;
+		QPoint lastMousePosition;
+		QQuaternion rotation;
+		QVector2D mousePressPosition;
+		QVector3D rotationAxis;
+		qreal angularSpeed;
+		double distance;
 };
 
 #endif // OGLWIDGET_HPP
