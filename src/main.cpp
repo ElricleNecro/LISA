e218fb1552ab1e732944309d025a51747b77621d
ElricleNecro <guiplum@gmail.com>
2014-01-31 19:51:33 +0100

    Now read a gadget file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include "mainwindow.h"
 #include "oglwidget.hpp"
+#include "gadget.hpp"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 
+	Gadget obj("None_000");
+	OGLWidget w(&obj);
+	w.show();
+	//x.show();
 
-    OGLWidget w, x;
-    w.show();
-    //x.show();
-
-    return a.exec();
+	return a.exec();
 }
