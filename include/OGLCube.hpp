e9200411463ded1ca3b0fc63e6e84a2f2b99b228
ElricleNecro <guiplum@gmail.com>
2014-01-31 19:49:30 +0100

    Remove some unused header + redo indentation.

--- include/OGLCube.hpp
+++ include/OGLCube.hpp
@@ -5,8 +5,6 @@
 #define GL3_PROTOTYPES 1
 #include <GL3/gl3.h>
 
-#include <stdexcept>
-
 #include <QGLShader>
 
 namespace OGLCube {
@@ -14,17 +12,17 @@ namespace OGLCube {
 		public:
 			OGLCube(void);
 			OGLCube(float taille, std::string VerticeShader, std::string FragShader);
-            virtual ~OGLCube(void);
+			virtual ~OGLCube(void);
 
-            void Show(QGLShaderProgram &m_program, QMatrix4x4 &model);
+			void Show(QGLShaderProgram &m_program, QMatrix4x4 &model);
 
-        private:
+		private:
 			void init(void);
 
 		protected:
 			// Vertices et indices
-            QVector<QVector3D> vertices;
-            QVector<QVector3D> colors;
+			QVector<QVector3D> vertices;
+			QVector<QVector3D> colors;
 	};
 };
 #endif /* end of include guard: OGLCUBE_H */
