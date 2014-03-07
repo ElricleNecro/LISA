1f132c8c9c2b52dc8833e841f19456e7556b981d
Acanthostega <acanthostega@hotmail.fr>
2014-03-07 23:34:43 +0100

    Passed the OGLWidget as a scene object, set then in the viewport set with other OpenGL viewport and making the viewport as current context for opengl

--- LISA/Figure.py
+++ LISA/Figure.py
@@ -4,112 +4,114 @@ import OGLWidget as og
 
 from PyQt5 import Qt, QtOpenGL as qo, QtGui as qg, QtCore as qc
 
+
 class Scene(Qt.QGraphicsScene):
-	def __init__(self, EventHandler, *args, **kwargs):
-		super(Scene, self).__init__(*args, **kwargs)
-		self._event_handler = EventHandler
-		self._timer = EventHandler.getTimer(self)
 
-	def wheelEvent(self, event):
-		if event.isAccepted():
-			return
-		self._event_handler.wheelEvent(self)
-		self.update()
+    def __init__(self, EventHandler, *args, **kwargs):
+        super(Scene, self).__init__(*args, **kwargs)
+        self._event_handler = EventHandler
+        self._timer = EventHandler.getTimer(self)
 
-	def mousePressEvent(self, event):
-		if event.isAccepted():
-			return
-		self._event_handler.mousePressEvent(event)
+    def wheelEvent(self, event):
+        if event.isAccepted():
+            return
+        self._event_handler.wheelEvent(self)
+        self.update()
 
-	def mouseMoveEvent(self, event):
-		if event.isAccepted():
-			return
-		if event.buttons() == qc.Qt.LeftButton:
-			self._event_handler.mouseMoveEvent(event)
+    def mousePressEvent(self, event):
+        if event.isAccepted():
+            return
+        self._event_handler.mousePressEvent(event)
 
-		self.update()
+    def mouseMoveEvent(self, event):
+        if event.isAccepted():
+            return
+        if event.buttons() == qc.Qt.LeftButton:
+            self._event_handler.mouseMoveEvent(event)
 
-	def mouseReleaseEvent(self, event):
-		if event.isAccepted():
-			return
-		self._event_handler.mouseReleaseEvent(event)
+        self.update()
 
-	def timerEvent(self, event):
-		self._event_handler.timerEvent(event)
-		self.update()
+    def mouseReleaseEvent(self, event):
+        if event.isAccepted():
+            return
+        self._event_handler.mouseReleaseEvent(event)
+
+    def timerEvent(self, event):
+        self._event_handler.timerEvent(event)
+        self.update()
+
 
 class Figure(Qt.QGraphicsView):
-	def __init__(self, *args, **kwargs):
-		super(Figure, self).__init__(*args, **kwargs)
 
-		# A color we need a lot of times:
-		self._color = Qt.QColor()
-		self._color.black()
+    def __init__(self, *args, **kwargs):
+        super(Figure, self).__init__(*args, **kwargs)
 
-		# Creation of the Plotting class:
-		self._axes  = og.OGLWidget()
-		# Then we add it as background of the View:
-		self.setViewport(self._axes)
-		self.setViewportUpdateMode(Qt.QGraphicsView.FullViewportUpdate)
+        # A color we need a lot of times:
+        self._color = Qt.QColor()
+        self._color.black()
 
-		# We create the scene object which will contain all widget:
-		self._scene = Scene(self._axes, self)
-		#self._scene = Qt.QGraphicsScene(self)
-		# And we set it as scene for the View:
-		self.setScene(self._scene)
+        # Creation of the Plotting class:
+        # Then we add it as background of the View:
+        # self.setViewport(self._axes)
+        context = qo.QGLWidget()
+        self.setViewport(context)
+        context.makeCurrent()
+        self.setViewportUpdateMode(Qt.QGraphicsView.FullViewportUpdate)
 
-		# Set some properties and palette to have a black background:
-		self.setAutoFillBackground(True)
-		self.setPalette(
-				qg.QPalette(
-					self._color
-				)
-		)
+        # And we set it as scene for the View:
+        self._axes = og.OGLWidget()
+        self._axes.createDialog()
+        self.setScene(self._axes)
+        self._axes.initializeGL()
 
-	def setupViewport(self, viewport):
-		viewport.makeCurrent()
-		self._axes.initializeGL()
-		viewport.doneCurrent()
+        # Set some properties and palette to have a black background:
+        self.setAutoFillBackground(True)
+        self.setPalette(
+            qg.QPalette(
+                self._color
+            )
+        )
+        context.doneCurrent()
 
-	def resizeEvent(self, event):
-		self._axes.resizeGL(event.size().width(), event.size().height())
+    def resizeEvent(self, event):
+        self._axes.resizeGL(event.size().width(), event.size().height())
 
-	def drawBackground(self, painter, rect):
-		self._axes.paintGL()
+    def keyPressEvent(self, event):
+        if (
+            event.modifiers() == qc.Qt.ControlModifier and
+            event.key() == qc.Qt.Key_W
+        ) or event.key() == qc.Qt.Key_Escape:
+            print("Quiting!")
+            self.close()
+        else:
+            event.ignore()
 
-	def keyPressEvent(self, event):
-		if (event.modifiers() == qc.Qt.ControlModifier and event.key() == qc.Qt.Key_W) or event.key() == qc.Qt.Key_Escape:
-			print("Quiting!")
-			self.close()
-		else:
-			event.ignore()
+    def __getitem__(self, ind):
+        return self._axes.lines[ind]
 
-	def __getitem__(self, ind):
-		return self._axes.lines[ind]
+    def __delitem__(self, ind):
+        pass
 
-	def __delitem__(self, ind):
-		pass
+    def newWidget(self, wid):
+        tmp = self._scene.addWidget(wid, qc.Qt.Window)
+        tmp.setFlag(
+            Qt.QGraphicsItem.ItemIsMovable
+        )
+        tmp.setCacheMode(
+            Qt.QGraphicsItem.DeviceCoordinateCache
+        )
 
-	def newWidget(self, wid):
-		tmp = self._scene.addWidget(wid, qc.Qt.Window)
-		tmp.setFlag(
-				Qt.QGraphicsItem.ItemIsMovable
-		)
-		tmp.setCacheMode(
-				Qt.QGraphicsItem.DeviceCoordinateCache
-		)
+    @property
+    def axes(self):
+        return self._axes.lines
 
-	@property
-	def axes(self):
-		return self._axes.lines
-	@axes.setter
-	def axes(self, value):
-		self._axes.lines = value
+    @axes.setter
+    def axes(self, value):
+        self._axes.lines = value
 
-		try:
-			wid = value.createWidget()
-			if wid:
-				self.newWidget(wid)
-		except AttributeError:
-			pass
-
+        try:
+            wid = value.createWidget()
+            if wid:
+                self.newWidget(wid)
+        except AttributeError:
+            pass
