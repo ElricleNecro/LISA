2b15cc5992df03a11150e3190f43d0e975aa5aea
Acanthostega <acanthostega@hotmail.fr>
2014-03-13 19:10:26 +0100

    Use directly the OpenGL interface for plotting points without memory leak. Set number of points to 100000.

--- LISA/__main__.py
+++ LISA/__main__.py
@@ -32,8 +32,8 @@ def cleanupCallback(context=None):
 class TestOGL(object):
 
     def __init__(self, *args, **kwargs):
-        rand = np.random.rand(1000, 3)
-        self._color = np.random.rand(1000, 3)
+        rand = np.random.rand(100000, 3)
+        self._color = np.random.rand(100000, 3).flatten()
 
         r = rand[:, 0] ** (1. / 3.)
         thet = np.arccos(2 * rand[:, 1] - 1)
@@ -45,18 +45,35 @@ class TestOGL(object):
                 r * np.sin(phi) * np.sin(thet),
                 r * np.cos(thet)
             ]
-        ).T
+        ).T.flatten()
 
     def show(self, shaders, matrice):
         shaders.setUniformValue("modelview", matrice)
 
-        shaders.setAttributeArray("in_Vertex", self._pos)
-        shaders.setAttributeArray("in_Color", self._color)
+        vertex_id = shaders.attributeLocation("in_Vertex")
+        color_id = shaders.attributeLocation("in_Color")
 
         shaders.enableAttributeArray("in_Vertex")
         shaders.enableAttributeArray("in_Color")
 
-        GL.glDrawArrays(GL.GL_POINTS, 0, self._pos.shape[0])
+        GL.glVertexAttribPointer(
+            vertex_id,
+            3,
+            GL.GL_DOUBLE,
+            GL.GL_FALSE,
+            0,
+            self._pos
+        )
+        GL.glVertexAttribPointer(
+            color_id,
+            3,
+            GL.GL_DOUBLE,
+            GL.GL_FALSE,
+            0,
+            self._color
+        )
+
+        GL.glDrawArrays(GL.GL_POINTS, 0, self._pos.shape[0] // 3)
 
         shaders.disableAttributeArray("in_Vertex")
         shaders.disableAttributeArray("in_Color")
