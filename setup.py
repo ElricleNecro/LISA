f47dc8ae2d18792877d4b7822f35bf34c569683b
ElricleNecro <guiplum@gmail.com>
2014-04-07 18:51:55 +0200

    Enhanced the setup.py to use cython.

--- setup.py
+++ setup.py
@@ -16,28 +16,64 @@ except ImportError:
 import setuptools as st
 from distutils.core import setup
 from distutils.command.install_data import install_data
+from Cython.Distutils.extension import Extension
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 packages = st.find_packages()
 
 #--------------------------------------------------------------------------------------------------------------
+# Setup for the cython part:
+#--------------------------------------------------------------------------------------------------------------
+
+## Needed function:
+def scandir(dir, files=[]):
+	for file in os.listdir(dir):
+		path = os.path.join(dir, file)
+		if os.path.isfile(path) and path.endswith(".pyx"):
+			files.append(path.replace(os.path.sep, ".")[:-4])
+		elif os.path.isdir(path):
+			scandir(path, files)
+	return files
+
+def makeExtension(extName, **kwargs):
+	extPath = [ extName.replace(".", os.path.sep)+".pyx" ]
+	return Extension(
+			extName,
+			extPath
+		)
+
+## Setup:
+extNames = scandir("LISA")
+
+extensions = []
+for name in extNames:
+	extensions.append(
+		makeExtension(
+			name,
+			cython_directives = {
+				"embedsignature" : True,
+			}
+		)
+	)
+
+#--------------------------------------------------------------------------------------------------------------
 # Call the setup function:
 #--------------------------------------------------------------------------------------------------------------
-print(glob.glob('Shaders/*') + glob.glob('Textures/*'))
 setup(
-	name        = 'LISA is a Simulation Analyzer',
-	version     = '0.1a',
-	description = 'Python Module for analysis simulation.',
-	author      = 'Guillaume Plum, Manuel Duarte',			# Sûrement pas bon, à changer.
+	name         = 'LISA is a Simulation Analyzer',
+	version      = '0.1a',
+	description  = 'Python Module for analysis simulation.',
+	author       = 'Guillaume Plum, Manuel Duarte',			# Sûrement pas bon, à changer.
 
-	cmdclass    = {'install_data': install_data},
+	cmdclass     = {'install_data': install_data, 'build_ext': build_ext},
 
-	packages    = packages,
-	package_data={'LISA': glob.glob('Shaders/*') + glob.glob('Textures/*')},
-
-	#data_files  = [
-		#('share/LibThese/animation-plugins', ["share/LibThese/animation-plugins/__init__.py"]), #glob.glob("share/LibThese/animation-plugins/*.py")),
-	#],
-	#scripts = [
-		#'scripts/animationv2.py'
-	#],
+	packages     = packages,
+	package_data = {'LISA': glob.glob('Shaders/*') + glob.glob('Textures/*')},
+	ext_modules  = cythonize(
+				extensions,
+				include_path = ['.']
+			),
 )
+
+#vim:spelllang=
