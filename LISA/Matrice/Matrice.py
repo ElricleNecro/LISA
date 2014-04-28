9f9d080b5732051a34e2c13f9f860fd102252f3a
ElricleNecro <guiplum@gmail.com>
2014-04-28 20:10:52 +0200

    Create some convenient interface to numpy.ndarray for vector and matrices.

---
+++ LISA/Matrice/Matrice.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import math as m
+import numpy as np
+
+from .Vector import Vector
+
+
+class Matrix(np.ndarray):
+
+    """An upper class over numpy.ndarray to deal with our module matrices."""
+
+    def __init__(self, *args, **kwargs):
+        print(len(self.shape))
+        if len(self.shape) == 2:
+            if self.shape[0] != self.shape[1]:
+                self._dim_str = str(self.shape[0]) + "x" + str(self.shape[1])
+            else:
+                self._dim_str = str(self.shape[0])
+
+            if self.dtype == np.float32:
+                self._dim_str += "f"
+            elif self.dtype == np.float64:
+                self._dim_str += "d"
+            self._dim_str = "glUniformMatrix" + self._dim_str + "v"
+        else:
+            raise ValueError("Wrong Matrices shape: " + str(len(self.shape)))
+
+        self[:] = np.zeros_like(self)
+
+    @property
+    def dim_str(self):
+        if len(self.shape) == 2:
+            if self.shape[0] != self.shape[1]:
+                self._dim_str = str(self.shape[0]) + "x" + str(self.shape[1])
+            else:
+                self._dim_str = str(self.shape[0])
+
+            if self.dtype == np.float32:
+                self._dim_str += "f"
+            elif self.dtype == np.float64:
+                self._dim_str += "d"
+            self._dim_str = "glUniformMatrix" + self._dim_str + "v"
+        else:
+            raise ValueError("Wrong Matrices shape: " + str(len(self.shape)))
+
+        return self._dim_str
+
+    def setToIdentity(self):
+        self[:] = 0.
+        self.ravel()[0::self.shape[1]+1] = 1.
+
+    def perspective(self, FoV, ratio, near, far):
+        self = self * Perspective(FoV, ratio, near, far)
+
+    def lookAt(self, pos, center, up):
+        self = self * LookAt(pos, center, up)
+
+    def __mul__(self, a):
+        res = np.dot(self, a)
+
+        mat = Matrix(res.shape, dtype=res.dtype)
+        mat[:, :] = res[:, :]
+
+        return mat
+
+    def __rmul__(self, a):
+        res = np.dot(a, self)
+
+        mat = Matrix(res.shape, dtype=res.dtype)
+        mat[:, :] = res[:, :]
+
+        return mat
+
+    def __imul__(self, a):
+        self = self * a
+        return self
+
+
+def Identity(shape=(4, 4), dtype=np.float64):
+    mat = Matrix(shape, dtype, order='C')
+    mat.setToIdentity()
+    return mat
+
+
+def Perspective(FoV, ratio, near, far, dtype=np.float64):
+    mat = Matrix((4, 4), dtype, order='C')
+    f = 1. / m.tan(FoV/2.0 * np.pi / 180.)
+    mat[0, 0] = f / ratio
+    mat[1, 1] = f
+    mat[2, 2] = (near + far) / (near - far)
+    mat[2, 3] = 2. * near * far / (near - far)
+    mat[3, 2] = -1.
+
+    return mat
+
+
+def LookAt(pos: Vector, center: Vector, up: Vector, dtype=np.float64):
+    mat = Matrix((4, 4), dtype, order='C')
+
+    regard = center - pos
+    normal = regard*up
+    n_axe = normal*regard
+
+    normal /= normal.norm()
+    n_axe /= n_axe.norm()
+    regard /= regard.norm()
+
+    mat[0, :3] = normal
+    mat[1, :3] = n_axe
+    mat[2, :3] = -regard
+    mat[3, 3] = 1.
+
+    return mat
+
+
+def Quaternion(angle, axe: Vector, dtype=None):
+    if not dtype:
+        dtype = axe.dtype
+    mat = Matrix((4, 4), dtype, order='C')
+
+    axe /= axe.norm()
+
+    mat[0, 0] = axe[0] * axe[0] * (1 - m.cos(angle)) + m.cos(angle)
+    mat[0, 1] = axe[0] * axe[1] * (1 - m.cos(angle)) - axe[2] * m.sin(angle)
+    mat[0, 2] = axe[0] * axe[2] * (1 - m.cos(angle)) + axe[1] * m.sin(angle)
+
+    mat[1, 0] = axe[0] * axe[1] * (1 - m.cos(angle)) + axe[2] * m.sin(angle)
+    mat[1, 1] = axe[1] * axe[1] * (1 - m.cos(angle)) + m.cos(angle)
+    mat[1, 2] = axe[1] * axe[2] * (1 - m.cos(angle)) - axe[0] * m.sin(angle)
+
+    mat[2, 0] = axe[0] * axe[2] * (1 - m.cos(angle)) - axe[1] * m.sin(angle)
+    mat[2, 1] = axe[1] * axe[2] * (1 - m.cos(angle)) + axe[0] * m.sin(angle)
+    mat[2, 2] = axe[2] * axe[2] * (1 - m.cos(angle)) + m.cos(angle)
+
+    mat[3, 3] = 1.0
+
+    return mat
---
+++ LISA/Matrice/Vector.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import numpy as np
+
+
+class Vector(np.ndarray):
+
+    """Vector dealing class"""
+    def __new__(cls, *args, dtype=np.float64, **kwargs):
+        buf = np.array(args, dtype=dtype)
+        if len(buf.shape) != 1:
+            raise ValueError(
+                "This class should only contain vector:" +
+                " len(shape) = %d != 1" % (
+                    len(buf.shape)
+                )
+            )
+        return super(Vector, cls).__new__(
+            cls,
+            buf.shape,
+            dtype=dtype,
+            buffer=buf,
+            order='C',
+            **kwargs
+        )
+
+    def __init__(self, *args, **kwargs):
+        self._dim_str = str(self.shape[0])
+
+        if self.dtype == np.float32:
+            self._dim_str += "f"
+        elif self.dtype == np.float64:
+            self._dim_str += "d"
+        self._dim_str = "glUniform" + self._dim_str + "v"
+
+    def __mul__(self, a):
+        if isinstance(a, Vector):
+            return Vector(*np.cross(self, a).tolist())
+        return Vector(
+            *(
+                super(Vector, self).__mul__(a)
+            ).tolist()
+        )
+
+    def __rmul__(self, a):
+        if isinstance(a, Vector):
+            return Vector(*np.cross(a, self).tolist())
+        return Vector(
+            *(
+                super(Vector, self).__rmul__(a)
+            ).tolist()
+        )
+
+    def __imul__(self, a):
+        self = self * a
+
+    def __lshift__(self, a):
+        return Vector(*np.dot(self, a).tolist())
+
+    def __rlshift(self, a):
+        return Vector(*np.dot(self, a).tolist())
+
+    def __ilshift__(self, a):
+        self = self << a
+
+    def norm(self):
+        return np.linalg.norm(self)
+
+    def normalized(self):
+        return Vector(*self.tolist()) / self.norm()
