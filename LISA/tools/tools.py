d6a8c9dd187b611502dfa8d92983da19dd5aadd5
Acanthostega <acanthostega@hotmail.fr>
2014-05-08 10:17:43 +0200

    Add tools for getting shaders and tecture from the data

--- LISA/tools/tools.py
+++ LISA/tools/tools.py
@@ -2,8 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from OpenGL import GL
+from LISA import common as c
 
 
+# dictionary for correspondence between numpy dtypes and OpenGL types
 DTYPE_TO_GL = dict(
 )
 DTYPE_TO_GL["float64"] = GL.GL_DOUBLE
@@ -15,5 +17,21 @@ DTYPE_TO_GL["uint16"] = GL.GL_UNSIGNED_SHORT
 DTYPE_TO_GL["int16"] = GL.GL_SHORT
 DTYPE_TO_GL["uint32"] = GL.GL_UNSIGNED_INT
 DTYPE_TO_GL["int32"] = GL.GL_INT
+
+
+# path for texture
+def texture_path(texture):
+    return c.os.path.join(
+        c.TEXTURE_DIR,
+        texture,
+    )
+
+
+# path for shaders
+def shader_path(shader):
+    return c.os.path.join(
+        c.SHADERS_DIR,
+        shader,
+    )
 
 # vim: set tw=79 :
