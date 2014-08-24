720b2a5927a6f871370f921efc9ae34b99f81d3f
ElricleNecro <guiplum@gmail.com>
2014-08-24 12:05:51 +0200

    Adaptation to the new Shaders wrapper and move all linetype classes to another file.

--- LISA/Object/Base.py
+++ LISA/Object/Base.py
@@ -3,136 +3,42 @@
 
 import numpy as np
 
-import LISA.tools.common as c
+import LISA.tools as t
 import LISA.Matrice as m
 
-from LISA.OpenGL import Shaders as s
+from .Meshtype import Point
+from LISA.OpenGL import Shaders
 from OpenGL import GL
 
 
 __all__ = [
-        "Point",
-        "Lines",
-        "TriangleMesh",
-        "QuadMesh",
         "Base",
 ]
 
 
-class Point(object):
-    def __call__(self, data):
-        GL.glDrawArrays(GL.GL_POINTS, 0, data.shape[0] // 3)
-
-
-class Lines(object):
-    def __init__(self, ids=None, data=None):
-        if (ids is None) and (data is not None):
-            l = len(data.flatten()) // 3
-            self._ids = np.array(
-                    range(l),
-                    dtype=np.uint32,
-            )
-            # self._ids = np.repeat(
-                    # range(l+1),
-                    # 2
-            # )[1:-3]
-            # self._ids[-1] = 0
-            # print(l, len(self._ids))
-        else:
-            self._ids = ids
-
-    def __call__(self, data):
-        GL.glDrawElements(
-            GL.GL_LINE_STRIP,
-            len(self._ids),
-            GL.GL_UNSIGNED_INT,
-            self._ids
-        )
-
-
-class TriangleMesh(object):
-    def __init__(self, ids=None, data=None, side_x=30, side_y=30):
-        if (ids is None) and (data is not None):
-            # create the indices for triangles
-            self._ids = np.empty(
-                (side_x - 1, side_y - 1, 6),
-                dtype=np.uint32
-            )
-            indices = np.array(range(side_x - 1), dtype=np.uint32)
-            for i in range(side_y - 1):
-                self._ids[:, i, 0] = indices[:] + i * side_y
-                self._ids[:, i, 1] = indices[:] + 1 + i * side_y
-                self._ids[:, i, 2] = indices[:] + (i + 1) * side_y
-                self._ids[:, i, 3] = indices[:] + (i + 1) * side_y
-                self._ids[:, i, 4] = indices[:] + 1 + (i + 1) * side_y
-                self._ids[:, i, 5] = indices[:] + 1 + i * side_y
-            self._ids = self._ids.flatten()
-        else:
-            self._ids = ids
-
-    def __call__(self, data):
-        # GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
-        GL.glDrawElements(
-            GL.GL_TRIANGLES,
-            len(self._ids),
-            # 6 * (self._len - 1) ** 2,
-            GL.GL_UNSIGNED_INT,
-            self._ids
-        )
-
-
-class QuadMesh(object):
-    def __init__(self, ids=None, data=None, side_x=30, side_y=30):
-        if (ids is None) and (data is not None):
-            # create the indices for triangles
-            self._ids = np.empty(
-                (side_x - 1, side_y - 1, 4),
-                dtype=np.uint32
-            )
-            indices = np.array(range(side_x - 1), dtype=np.uint32)
-            for i in range(side_y - 1):
-                self._ids[:, i, 0] = indices[:] + i * side_y
-                self._ids[:, i, 1] = indices[:] + 1 + i * side_y
-                self._ids[:, i, 2] = indices[:] + 1 + (i + 1) * side_y
-                self._ids[:, i, 3] = indices[:] + (i + 1) * side_y
-            self._ids = self._ids.flatten()
-        else:
-            self._ids = ids
-
-    def __call__(self, data):
-        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
-        GL.glDrawElements(
-            GL.GL_QUADS,
-            len(self._ids),
-            GL.GL_UNSIGNED_INT,
-            self._ids
-        )
-
-
 class Base(object):
-    def __init__(self, data, linetype=Point()):
+    def __init__(self, data, linetype=Point(), shaders=None):
         self.data = data
 
         self._model = m.Identity()
 
         self._plot_prop = linetype
 
-    def createShaders(self, parent):
-        self._shaders = s.CreateShaderFromFile(
-            c.os.path.join(
-                c.SHADERS_DIR,
-                "basic.vsh"
-            )
-        ) + s.CreateShaderFromFile(
-            c.os.path.join(
-                c.SHADERS_DIR,
-                "basic.fsh"
-            )
-        )
+        self._shaders = Shaders()
+        if shaders is not None:
+            for v in shaders:
+                self._shaders += v
 
+    def createShaders(self, parent):
+        if len(self._shaders) == 0:
+            self._shaders += t.shader_path("basic.vsh")
+            self._shaders += t.shader_path("basic.fsh")
         self._shaders.link()
 
     def show(self, parent):
+        if self._modified_data:
+            # self._plot_prop.init()
+            pass
         GL.glEnable(GL.GL_DEPTH_TEST)
 
         GL.glClear(GL.GL_DEPTH_BUFFER_BIT | GL.GL_COLOR_BUFFER_BIT)
@@ -155,9 +61,11 @@ class Base(object):
 
     @property
     def data(self):
+        self._modified_data = True
         return self._data
     @data.setter
     def data(self, val):
+        self._modified_data = True
         if len(val.shape) != 1:
             self._data = val.flatten()
         else:
@@ -166,10 +74,13 @@ class Base(object):
     @property
     def model(self):
         return self._model
-
     @model.setter
     def model(self, model):
         self._model = model
+
+    @property
+    def shaders(self):
+        return self._shaders
 
     def __lshift__(self, inst):
         self._plot_prop = inst
