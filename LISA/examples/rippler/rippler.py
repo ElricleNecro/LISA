c77fdeb6881241737f0f34e2e34b65b8e585cae0
ElricleNecro <guiplum@gmail.com>
2014-08-24 12:02:56 +0200

    Adaptation to the New Shaders wrapper.

--- LISA/examples/rippler/rippler.py
+++ LISA/examples/rippler/rippler.py
@@ -9,57 +9,39 @@ from OpenGL.arrays import numpymodule
 
 import LISA.tools as t
 import LISA.Matrice as m
+import LISA.Object as o
 
 from LISA.OpenGL import Buffer, INDEX_BUFFER, VERTEX_BUFFER
-from LISA.OpenGL import Shaders as s
+from LISA.OpenGL import Shaders
 from LISA.Matrice import Vector
 
 numpymodule.NumpyHandler.ERROR_ON_COPY = True
 
 
-class Rippler(object):
+class Rippler(o.Base):
 
     def __init__(self, *args, **kwargs):
 
-        # create mesh
-        self.npoints = 30
-        X = np.linspace(-1, 1, self.npoints)
-        Y = np.linspace(-1, 1, self.npoints)
-        Z = np.zeros(self.npoints, dtype=np.float32)
-        x, y, z = np.meshgrid(X, Y, Z)
-        self._mesh = np.array([x, y, z], dtype=np.float32).T.flatten()
-        print(self._mesh.shape)
+        npoints = 30
+        X = np.linspace(-1, 1, npoints).astype(np.float32)
+        Y = np.linspace(-1, 1, npoints).astype(np.float32)
+        Z = np.zeros((npoints,npoints), dtype=np.float32)
+        x, y = np.meshgrid(X, Y)
+        mesh = np.vstack((x, y, Z)).reshape(3, -1).T.astype(np.float32)
 
-        # create the indices for triangles
-        self._indices = np.empty(
-            (self.npoints - 1, self.npoints - 1, 6),
-            dtype=np.uint32
-        )
-        indices = np.array(range(self.npoints - 1), dtype=np.uint32)
-        for i in range(self.npoints - 1):
-            self._indices[i, :, 0] = indices[:] + i * self.npoints
-            self._indices[i, :, 1] = indices[:] + 1 + i * self.npoints
-            self._indices[i, :, 2] = indices[:] + (i + 1) * self.npoints
-            self._indices[i, :, 3] = indices[:] + (i + 1) * self.npoints
-            self._indices[i, :, 4] = indices[:] + 1 + (i + 1) * self.npoints
-            self._indices[i, :, 5] = indices[:] + 1 + i * self.npoints
-        self._indices = self._indices.flatten()
+        super(Rippler, self).__init__(mesh, linetype=o.TriangleMesh(data=mesh))
 
-        self._model = m.Identity()
+        self._shaders += t.shader_path("rippler/rippler.vsh")
+        self._shaders += t.shader_path("rippler/rippler.fsh")
 
         self._time = datetime.datetime.now()
 
     def createShaders(self, parent):
-
-        self._shaders = s.CreateShaderFromFile(
-            t.shader_path("rippler/rippler.vsh")
-        ) + s.CreateShaderFromFile(
-            t.shader_path("rippler/rippler.fsh")
-        )
-
-        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
-
-        self._shaders.link()
+        try:
+            super(Rippler, self).createShaders(parent)
+        except Exception as e:
+            print("WTF!!!!!!!!!!!\t", e)
+            raise e
 
         # create buffers
         self._vertices = Buffer(VERTEX_BUFFER)
@@ -68,48 +50,56 @@ class Rippler(object):
         self._index.create()
 
         # allocate buffers
+        print(self._data.shape)
         self._vertices.bind()
         self._vertices.allocate(
-            self._mesh,
-            len(self._mesh) * 4
+            self._data,
+            len(self._data) * 4
         )
         self._vertices.release()
         self._index.bind()
         self._index.allocate(
-            self._indices,
-            len(self._indices) * 4
+            self._plot_prop._ids,
+            len(self._plot_prop._ids) * 4
         )
         self._index.release()
 
     def show(self, parent):
+        GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
         self._shaders.bind()
 
         self._shaders.setUniformValue(
+            "projection",
+            parent._projection
+        )
+        self._shaders.setUniformValue(
             "modelview",
-            parent._projection * parent._view * self._model
+            parent._view * self._model
         )
         dt = datetime.datetime.now() - self._time
         second = float((dt.seconds * 1000000 + dt.microseconds) * 0.000006)
         self._shaders.setUniformValue("time", Vector(second, dtype=np.float32))
 
         self._vertices.bind()
-        self._shaders.enableAttributeArray("in_Vertex")
+        self._shaders.enableAttributeArray("position")
         self._shaders.setAttributeBuffer(
-            "in_Vertex",
-            self._mesh,
+            "position",
+            self._data,
         )
         self._vertices.release()
 
         self._index.bind()
+
         GL.glDrawElements(
             GL.GL_TRIANGLES,
-            6 * (self.npoints - 1) ** 2,
+            len(self._plot_prop._ids),
             GL.GL_UNSIGNED_INT,
-            None
+            None,
         )
+
         self._index.release()
 
-        self._shaders.disableAttributeArray("in_Vertex")
+        self._shaders.disableAttributeArray("position")
         self._shaders.release()
 
 # vim: set tw=79 :
