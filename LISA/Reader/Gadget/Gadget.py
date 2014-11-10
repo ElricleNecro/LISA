6af03294788ac685fdb7169ba8fbed712970a5c5
Guillaume Plum <guiplum@gmail.com>
2014-11-10 15:10:40 +0100

    Add a few accessor to the Simu class to deal with different shader parameter

--- LISA/Reader/Gadget/Gadget.py
+++ LISA/Reader/Gadget/Gadget.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# import numpy as np
+import numpy as np
 import LISA.tools.tools as c
 
 from OpenGL import GL
@@ -25,11 +25,33 @@ class Simu(o.Base):
         fich = GadgetReader(filename, num)
         fich.Read()
 
+        print(fich.npart)
+
+        if "type" in kwargs:
+            ntype = kwargs["type"]
+            start = sum( fich.npart[:ntype] )
+            end = start + fich.npart[ntype]
+            pos = fich.positions[3*start:3*end].astype(np.float32)
+            self._type = kwargs["type"]
+
+            del kwargs["type"]
+        else:
+            self._type = None
+            pos = fich.positions.astype(np.float32)
+
+        if "BoxSize" in kwargs:
+            if kwargs["BoxSize"]:
+                pos[:] = pos[:] - fich.BoxSize / 2.
+            del kwargs["BoxSize"]
+
+        self._filename = filename
         self._sigma = 0.2
         self._size = 0.01
         self._cut_off = 0.25
+        self._scale = 100.0
+        self._color = m.Vector(1., 1., 1.)
 
-        super(Simu, self).__init__(fich.positions, **kwargs)
+        super(Simu, self).__init__(pos, **kwargs)
 
         self._shaders += c.shader_path("halo/halo.vsh")
         self._shaders += c.shader_path("halo/halo.fsh")
@@ -38,24 +60,23 @@ class Simu(o.Base):
         self._shaders.link()
 
         GL.glEnable(GL.GL_PROGRAM_POINT_SIZE)
-        GL.glEnable(GL.GL_POINT_SPRITE)
         GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
         GL.glEnable(GL.GL_BLEND)
-        # GL.glEnable(GL.DEPTH_TEST)
+        GL.glEnable(GL.DEPTH_TEST)
 
     def show(self, parent):
-
-        GL.glClear(GL.GL_DEPTH_BUFFER_BIT | GL.GL_COLOR_BUFFER_BIT)
-
         matrice = parent._view * self._model
 
         self._shaders.bind()
+
         self._shaders.setUniformValue("modelview", matrice)
         self._shaders.setUniformValue("projection", parent._projection)
-        self._shaders.setUniformValue("screenSize", parent._screensize)
+        # self._shaders.setUniformValue("screenSize", parent._screensize)
+        self._shaders.setUniformValue("scale", m.Vector(self._scale))
         self._shaders.setUniformValue("voxelSize", m.Vector(self._size))
         self._shaders.setUniformValue("sigma", m.Vector(self._sigma))
         self._shaders.setUniformValue("cut_off", m.Vector(self._cut_off))
+        self._shaders.setUniformValue("in_color", self._color)
 
         self._shaders.enableAttributeArray("position")
 
